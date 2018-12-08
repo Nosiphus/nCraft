df17e9a72676f5de2600659b9a3e8bdb455e481a
Sean Crain <38871906+seancrain@users.noreply.github.com>
2018-12-08 16:09:48 -0600

    Entirely restructure nCraft

--- src/ncraft/core/Window.java
+++ src/ncraft/core/Window.java
@@ -6,6 +6,17 @@ import ncraft.core.buttons.*;
 
 public class Window {
 	
+	public class OSDetection {   
+	    public void windows(String[] args) {          
+	        String os = System.getProperty("os.name");        
+	        if(os.contains("Windows")) {  
+	            System.out.println("nCraft is running on Windows.");  
+	        }else  
+	            System.out.println("nCraft requires Windows.");
+	        	System.exit(0);
+	    }  
+	}  
+	
 	JFrame launcher;
 	
 	Window() {
@@ -14,12 +25,6 @@ public class Window {
 		JLabel label = new JLabel(jack);
 		
 		//Buttons
-		
-			//Check for Updates
-			JButton checkUpdates = new JButton("Check for Updates");
-			checkUpdates.setBounds(610, 460, 140, 30);
-			checkUpdates.addActionListener(new UpdateListener());
-			checkUpdates.setToolTipText("Checks for updates.");
 			
 			//Download
 			JButton download = new JButton("Download");
@@ -32,14 +37,20 @@ public class Window {
 			login.setBounds(870, 460, 70, 30);
 			login.addActionListener(new LoginListener());
 			login.setToolTipText("Will eventually log player into Minecraft.");
+			
+			//Options
+			JButton options = new JButton("Options");
+			options.setBounds(660, 460, 90, 30);
+			options.addActionListener(new OptionsListener());
+			options.setToolTipText("Opens Options Menu.");
 		
 		//End Buttons
 		
 		//Window Contents
 		launcher.add(label);
-		launcher.add(checkUpdates);
 		launcher.add(download);
 		launcher.add(login);
+		launcher.add(options);
 		
 		//Window Operations
 		launcher.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
---
+++ src/ncraft/core/buttons/OptionsListener.java
@@ -0,0 +1,11 @@
+package ncraft.core.buttons;
+
+import java.awt.event.*;
+import javax.swing.*;
+
+public class OptionsListener implements ActionListener {
+	public void actionPerformed(ActionEvent e)
+	{
+		JOptionPane.showMessageDialog(null, "It's hip to fuck bees!");
+	}
+}
